8153f2370cc28d4530ea615e4b0ea676282dbe49
jrrdl1506 <128421660+jrrdl1506@users.noreply.github.com>
2024-03-04 11:49:53 -0600

    api

---
+++ src/app/services/global-api.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GlobalApiService {
+  private apiUrl: string = 'localhost:4000/dasHaus';
+  constructor() { }
+
+  getApiURL(){
+    return this.apiUrl;
+  }
+}
