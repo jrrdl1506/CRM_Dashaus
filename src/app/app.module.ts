9192e0bfaed37c6d10d265e0184506ecd367b741
jrrdl1506 <128421660+jrrdl1506@users.noreply.github.com>
2024-03-04 13:14:51 -0600

    bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +39,8 @@ import { InventarioDisponibleComponent } from './components/graficas/inventario-
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    NgxChartsModule
+    NgxChartsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
