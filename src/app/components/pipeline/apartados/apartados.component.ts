3c43edd27758d521c07cfcd3461d325443a53fb3
jrrdl1506 <128421660+jrrdl1506@users.noreply.github.com>
2024-03-13 13:05:55 -0600

    cambios

--- src/app/components/pipeline/apartados/apartados.component.ts
+++ src/app/components/pipeline/apartados/apartados.component.ts
@@ -12,6 +12,8 @@ export class ApartadosComponent {
   enganche: number=0;
   plazo: number=0;
 
+  arrMensualidades:any[]=[];
+
   constructor(){
 
   }
@@ -42,8 +44,157 @@ export class ApartadosComponent {
   }
 
   calcularPlazo(){
-    var plazos = this.precio / this.plazo;
-    console.log(plazos,"plazos");
+    
+    if(this.plazo == null || this.plazo == 0 || this.plazo < 0){
+      
+    }
+    else{
+      this.colocarLosPlazos();
+    }
+    // var precioCadaPlazo = this.precio / this.plazo;
+    // console.log(precioCadaPlazo,"plazos");
+    // alert(this.plazo);
+    
+    
   }
+
+  colocarLosPlazos(){
+
+    const fechaInicio = new Date(2024, 0, 1); // 1 de enero de 2024
+   
+    const mensualidades = this.generarMensualidades(this.plazo, fechaInicio);
+    
+    this.generarMensualidades(this.plazo,new Date());
+    console.log(this.arrMensualidades);
+    
+  
+    this.colocarTds();
+
+    // var tbody = document.getElementById('trBody');
+    // if (tbody) {
+    //   tbody.innerHTML = ""; // Limpiar el contenido actual del tbody
+
+    //   alert(this.plazo);
+
+    //   for (var i = 0; i < this.plazo; i++) {
+    //     var newRow = document.createElement('tr'); // Crear una nueva fila
+        
+    //     for (var j = 0; j < 6; j++) { // Crear cuatro celdas 'td' en cada fila
+    //       var newTd = document.createElement('td'); // Crear un nuevo elemento td
+    //       newTd.textContent = 'a'; // Asignar un contenido al td (en este caso, 'a')
+    //       newRow.appendChild(newTd); // Agregar el nuevo td a la fila
+    //     }
+        
+    //     tbody.appendChild(newRow); // Agregar la nueva fila al tbody
+    //   }
+    // }
+
+  }
+
+  generarMensualidades(numeroMensualidades: number, fechaInicio: Date): Date[] {
+    this.arrMensualidades = [];
+    const mensualidades: Date[] = [];
+    let fechaActual: Date = new Date(fechaInicio);
+    
+    console.log(this.precio + " " + this.plazo,"precio y plazo");
+    for (let i = 0; i < numeroMensualidades; i++) {
+        mensualidades.push(new Date(fechaActual));
+        fechaActual.setMonth(fechaActual.getMonth() + 1);
+        var mensualidad = {
+          periodo:i,
+          pago:this.precio/this.plazo,
+          fechaPago:fechaActual.setMonth(fechaActual.getMonth() + 1),
+          saldo:100
+        }
+        this.arrMensualidades.push(mensualidad);
+    }
+
+    return mensualidades;
+  }
+
+
+
+
+ imprimirFechas(fechas: Date[]): void {
+    fechas.forEach((fecha, index) => {
+        console.log(`Mensualidad ${index + 1}: ${fecha.toLocaleDateString()}`);
+    });
+  }
+
+
+  colocarTds(){
+    var tbody = document.getElementById('trBody');
+    const periodoTd = document.getElementById('c_periodo');
+    const pagoTd = document.getElementById('c_pago');
+    const fechaTd = document.getElementById('c_fecha');
+    const interesTd = document.getElementById('c_interes');
+    const capitalTd = document.getElementById('c_capital');
+    const saldoTd = document.getElementById('c_saldo');
+
+    
+    if(tbody){
+    
+      while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild);
+      }
+    }
+    
+
+    if( tbody){
+     
+
+      // periodo:i,
+      // pago:this.precio/this.plazo,
+      // fechaPago:fechaActual.setMonth(fechaActual.getMonth() + 1),
+      // saldo:100
+
+      
+      for (var i = 0; i < this.plazo; i++) {
+        var newRow = document.createElement('tr'); // Crear una nueva fila
+      
+        var newTd1 = document.createElement('td'); // Crear un nuevo elemento td
+        newTd1.textContent = this.arrMensualidades[i].periodo; // Asignar un contenido al td
+        newRow.appendChild(newTd1); // Agregar el nuevo td a la fila
+  
+        var newTd2 = document.createElement('td'); // Crear un nuevo elemento td
+        newTd2.textContent = this.arrMensualidades[i].pago; // Asignar un contenido al td
+        newRow.appendChild(newTd2); // Agregar el nuevo td a la fila
+  
+        var newTd3 = document.createElement('td'); // Crear un nuevo elemento td
+        newTd3.textContent = this.arrMensualidades[i].fechaPago; // Asignar un contenido al td
+        newRow.appendChild(newTd3); // Agregar el nuevo td a la fila
+  
+        var newTd4 = document.createElement('td'); // Crear un nuevo elemento td
+        newTd4.textContent = 'Texto para interés'; // Asignar un contenido al td
+        newRow.appendChild(newTd4); // Agregar el nuevo td a la fila
+  
+        var newTd5 = document.createElement('td'); // Crear un nuevo elemento td
+        newTd5.textContent = 'Texto para capital'; // Asignar un contenido al td
+        newRow.appendChild(newTd5); // Agregar el nuevo td a la fila
+  
+        var newTd6 = document.createElement('td'); // Crear un nuevo elemento td
+        newTd6.textContent = this.arrMensualidades[i].saldo; // Asignar un contenido al td
+        newRow.appendChild(newTd6); // Agregar el nuevo td a la fila
+  
+        tbody.appendChild(newRow); // Agregar la nueva fila al tbody
+
+
+
+      }
+    }
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
